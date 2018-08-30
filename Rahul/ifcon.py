8e78a0a88e5418eb682b6881794a3a8bf19c92aa
Tim <erikersay@gmail.com>
2018-08-30 17:35:04 -0400

    Python Basics

---
+++ Rahul/ifcon.py
@@ -0,0 +1,16 @@
+
+var=123
+
+if (var==123):
+    print("Correct! The number is ",var)
+else:
+    print("Not Correct!")
+
+print("########################")
+
+str = "Welcome"
+if "s" in str:
+    print("Sucessful")
+else:
+    print("fail")
+print
