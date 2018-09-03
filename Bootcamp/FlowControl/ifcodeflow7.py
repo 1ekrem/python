069703db42e01f4d95bcdf498836d42ee68ca965
Tim <erikersay@gmail.com>
2018-09-03 15:53:10 -0400

    challenge

---
+++ Bootcamp/FlowControl/ifcodeflow7.py
@@ -0,0 +1,12 @@
+print(not False)
+print(not True)
+
+parrot="Turkish Delight"
+
+letter= input("Enter a character please.\n")
+
+#for i in parrot:
+if letter in letter:
+    print("-Give me an {}, Ekrem".format(letter))
+else:
+    print("None in the parrot string.")
---
+++ Bootcamp/Projects/SimpleConditionChallenge.py
@@ -0,0 +1,9 @@
+x=5
+y=7
+
+if x > y:
+    print("x is greater than y")
+elif x < y:
+    print("x is smaller than y")
+else:   # Don't write any condition in the else statement
+    print("x equals y")
