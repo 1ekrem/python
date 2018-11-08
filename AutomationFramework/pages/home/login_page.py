769fe94394ca6b73f132e3096358758dfe622644
Tim <erikersay@gmail.com>
2018-11-08 15:27:17 -0500

    11/08

--- AutomationFramework/pages/home/login_page.py
+++ AutomationFramework/pages/home/login_page.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from AutomationFramework.base.selenium_driver import SeleniumDriver
+from AutomationFramework.utilities.custom_logger import customLogger
+import logging
 
-class LoginPage():
+class LoginPage(SeleniumDriver):
     
+    log = customLogger(logging.DEBUG)
+
     def __init__(self, driver):
         super().__init__(driver)
         self.driver = driver
@@ -14,29 +18,17 @@ class LoginPage():
     _password_field = "user_password"
     _login_button = "commit"
 
-    def getLoginLink(self):
-        return self.driver.find_element (By.LINK_TEXT, self._login_link)
-    
-    def getEmailField(self):
-        return self.driver.find_element (By.ID, self._emai_field)
-
-    def getPasswordField(self):
-        return self.driver.find_element (By.ID, self._password_field)
-    
-    def getLoginButton(self):
-        return self.driver.find_element (By.NAME, self._login_button)
-
     def clickLoginLink(self):
-        self.getLoginLink().click()
+        self.elementClick(self._login_link, locatorType="link")
     
     def enterEmail(self, email):
-        self.getEmailField().send_keys(email)
+        self.sendKeys(email, self._emai_field)
     
     def enterPassword(self, password):
-        self.getPasswordField().send_keys(password)
+        self.sendKeys(password,self._password_field)
     
     def clickLoginButton(self):
-        self.getLoginButton().click()
+        self.elementClick(self._login_button, locatorType="name")
 
     def login(self, email, password):
         self.clickLoginLink()
@@ -44,12 +36,8 @@ class LoginPage():
         self.enterPassword(password)
         self.clickLoginButton()
 
-        #Based on the hard coding we create our APIs
-        # emailField = self.driver.find_element (By.ID, "user_email")
-        # emailField.send_keys(username)
+    def verifyLoginSuccessful(self):
+        result = self.isElementPresent(".//*[@id='navbar']//span[text()='Test User']", locatorType="xpath")
+        
+        return result
 
-        # passwordField = self.driver.find_element (By.ID, "user_password")
-        # passwordField.send_keys(password)
-
-        # loginButton = self.driver.find_element (By.NAME, "commit")
-        # loginButton.click()
--- AutomationFramework/tests/home/login_tests.py
+++ AutomationFramework/tests/home/login_tests.py
@@ -8,23 +8,18 @@ class LoginTests(unittest.TestCase):
     def test_validLogin(self):
         baseUrl = "https://learn.letskodeit.com/"
         driver = webdriver.Firefox()
-        #driver.maximize_window()
+        driver.maximize_window()
         driver.implicitly_wait(15)
         driver.get(baseUrl)
 
         lp = LoginPage(driver)
         lp.login("test@email.com", "abcabc")
 
-        coursesLink = "https://learn.letskodeit.com/courses"
-        driver.get(coursesLink)
+        # coursesLink = "https://learn.letskodeit.com/courses"
+        # driver.get(coursesLink)
 
-        userIcon = driver.find_element_by_xpath(".//*[@id='navbar']//span[text()='Test User']")
-        if userIcon is not None:
-            print("--Login Successful")
-        else:
-            print("--Login Failed")
-        
+        testresult = lp.verifyLoginSuccessful()
+
+        assert testresult == False
+
         driver.close()
-
-# ff = LoginTests()
-# ff.test_validLogin()
