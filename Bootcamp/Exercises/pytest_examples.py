84ef6e04e2a7ef69c7e147e7ba3e9090e2863a68
Tim <erikersay@gmail.com>
2018-12-04 10:08:04 -0500

    pytest.py examples

--- Bootcamp/Exercises/pytest_examples.py
+++ Bootcamp/Exercises/pytest_examples.py
@@ -3,9 +3,20 @@ import pytest
 def gtzero(number):
     return number >= 0
 
-def test_greater_than_zero():
+def test_tc1():
     assert gtzero(5) is True
-    # assert gtzero(-1) is True
+
+def test_tc2():
+    assert gtzero(-1) is True
+
+def test_tc3():
     assert gtzero(3) is True
+
+def test_tc4():
     assert gtzero(2) is True
-    # assert gtzero(-5) is True
+
+def test_tc5():    
+    assert gtzero(-5) is False
+
+def test_tc6():
+    assert 1 == 1
