bbe9d669c5adac0e601e773dd723331f701c147b
Tim <erikersay@gmail.com>
2019-01-11 11:53:42 -0500

    Interview Q - Word counting script

---
+++ interview_questions/count_word.py
@@ -0,0 +1,12 @@
+word = 'kjsh acvs kiwi ajsx xakj fkhds kajsx'
+
+count1=0
+count2=0
+word1='f'
+word2='s'
+for i in word:
+    if i == word1:
+        count1 += 1
+    elif i == word2:
+        count2 += 1
+print('Count of {}: '.format(word1),count1, '\n' 'Count of {}: '.format(word2), count2)
