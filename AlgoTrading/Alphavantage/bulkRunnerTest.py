6ad54f2ca40f8bd6cb0c53e7d22de646bf6d7a41
1ekrem <ersekrem@gmail.com>
2023-07-29 22:19:52 -0400

    versions merged

---
+++ AlgoTrading/Alphavantage/bulkRunnerTest.py
@@ -0,0 +1,21 @@
+from technicalAnalysisFunction import bulkRunner
+from NDX100 import NDX100
+from SPX import SPX
+
+file_path = 'C:\\Users\\Ekrem.Ersayin\\Documents\\pythonwork\\USTickers\\USMarketTicker-2023-07-19-Capture.csv'
+
+
+# with open(file_path, 'r') as file:
+#     # Assuming each value is on a separate line in the file
+#     for line_number, line in enumerate(file, start=1):
+#         value = line.strip()  # Remove leading/trailing whitespaces and newlines
+#         try:
+#             bulkRunner(value)
+#         except IndexError:
+#             pass    
+
+for ticker in NDX100:
+    try:
+        bulkRunner(ticker)
+    except IndexError:
+        pass    
---
+++ AlgoTrading/Alphavantage/getUSTickers.py
@@ -0,0 +1,22 @@
+import requests
+import csv
+from alphaVentageKey import keyValue
+from usEquityMarketTickers import equityTickers
+from datetime import date
+
+CSV_URL = 'https://www.alphavantage.co/query?function=LISTING_STATUS&apikey=keyValue'
+todayDate = date.today()
+
+with requests.Session() as s:
+    download = s.get(CSV_URL)
+    decoded_content = download.content.decode('utf-8')
+    cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+    my_list = list(cr)
+    print(my_list)
+    
+    USMarketTickers = "C:\\Users\\Ekrem.Ersayin\\Documents\\pythonwork\\USTickers\\USMarketTicker-{}.csv".format(todayDate)
+    with open (USMarketTickers, 'w') as list_file:
+        for row in my_list:
+            row = row[0]
+            list_file.write(str(row) + '\n')
+            print(row)
